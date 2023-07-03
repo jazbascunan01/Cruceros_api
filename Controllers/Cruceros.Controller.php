849edf13bc6d9ee79acc0b18fecf1da580a740cc
Jazmin Bascuñan <jazbascunan01@gmail.com>
2023-07-03 15:59:08 -0300

    Paginado, filtrado y ordenado funcionando en cruceros

--- Controllers/Cruceros.Controller.php
+++ Controllers/Cruceros.Controller.php
@@ -7,9 +7,76 @@ class CrucerosController extends ApiController
 {
     public function getCruceros($params = null)
     {
-        $cruceros = $this->crucerosmodel->getCruceros();
-        $this->crucerosview->response($cruceros, 200);
+        if (isset($_REQUEST['criterio'])) {
+            $cruceros = $this->GetSortedCruises($_REQUEST['criterio']);
+        } else {
+            if (isset($_REQUEST['filtrar'])) {
+                $cruceros = $this->GetFilteredCruises($_REQUEST['filtrar']);
+            } else {
+                if (isset($_REQUEST['pagina']) && isset($_REQUEST['filas'])) {
+                    $cruceros = $this->paginate($_REQUEST['pagina'], $_REQUEST['filas']);
+                    $this->crucerosview->response($cruceros, 200);
+                } else {
+                    $cruceros = $this->crucerosmodel->getAllCruceros();
+                    $this->crucerosview->response($cruceros, 200);
+                }
+            }
+        }
     }
+    public function GetSortedCruises($criterio)
+    {
+        if ($this->verifyAttributes($criterio)) {
+            if (isset($_REQUEST['orden']) && !empty($_REQUEST['orden'])) {
+                $orden = $_REQUEST['orden'];
+                $cruceros = $this->crucerosmodel->GetSortedCruceros($criterio, $orden);
+                $this->crucerosview->response($cruceros, 200);
+            } else {
+                $orden = "ASC"; // se ordena ascendente por defecto
+                $cruceros = $this->crucerosmodel->GetSortedCruceros($criterio, $orden);
+                $this->crucerosview->response($cruceros, 200);
+            }
+        } else {
+            return $this->crucerosview->response("Verificar la columna/atributo de la tabla elegida como criterio", 404);
+        }
+    }
+
+    public function GetFilteredCruises($filtro)
+    {
+        if ($this->verifyAttributes($filtro) && isset($_REQUEST['valor'])) {
+            $cruceros = $this->crucerosmodel->GetFilteredCruceros($filtro, $_REQUEST['valor']);
+            if ($cruceros) {
+                $this->crucerosview->response($cruceros, 200);
+            } else {
+                $this->crucerosview->response("No se encontraron cruceros filtrados", 404);
+            }
+        } else {
+            return $this->crucerosview->response("Verificar el filtro y el valor del filtro", 400);
+        }
+    }
+
+    public function paginate($pagina, $filas)
+    {
+        if (!empty($pagina) && !empty($filas) && $pagina > 0 && $filas > 0 && is_numeric($pagina) && is_numeric($filas)) {
+            $cantidad = $this->crucerosmodel->getTotalRecords();
+            if ($pagina <= $cantidad / $filas) {
+                $inicio = $filas * ($pagina - 1);
+                $cruceros = $this->crucerosmodel->getPaginatedCruceros($inicio, $filas);
+                return $cruceros;
+            } else {
+                return $this->crucerosview->response("La página solicitada con esa cantidad de filas no contiene elementos", 404);
+            }
+        } else {
+            return $this->crucerosview->response("Verificar la forma de los parámetros utilizados", 404);
+        }
+    }
+
+    public function verifyAttributes($filtro)
+    {
+        $atributos = $this->crucerosmodel->obtenerColumnas();
+        return (in_array($filtro, array_column($atributos, 'column_name')));
+    }
+
+
 
     public function getCrucero($params = null)
     {
@@ -26,7 +93,7 @@ class CrucerosController extends ApiController
     {
         $crucero = $this->getData(); // la obtengo del body
         // inserta el crucero
-        $crucero_id = $this->crucerosmodel->save($crucero->nombre, $crucero->compania, $crucero->capacidad, $crucero->origen, $crucero->img1, $crucero->img2,  $crucero->descripcion, $crucero->detalles);
+        $crucero_id = $this->crucerosmodel->save($crucero->nombre, $crucero->compania, $crucero->capacidad, $crucero->origen, $crucero->img1, $crucero->img2, $crucero->descripcion, $crucero->detalles);
 
         // obtengo el crucero recien creado
         $cruceroNuevo = $this->crucerosmodel->getCrucero($crucero_id);
@@ -57,14 +124,14 @@ class CrucerosController extends ApiController
         if ($crucero) {
             $body = $this->getData();
 
-            $nombre= $body->nombre;
-            $compania= $body->compania;
-            $capacidad= $body->capacidad;
-            $origen= $body->origen;
-            $img1= $body->img1;
-            $img2= $body->img2;
-            $descripcion= $body->descripcion;
-            $detalles= $body->detalles;
+            $nombre = $body->nombre;
+            $compania = $body->compania;
+            $capacidad = $body->capacidad;
+            $origen = $body->origen;
+            $img1 = $body->img1;
+            $img2 = $body->img2;
+            $descripcion = $body->descripcion;
+            $detalles = $body->detalles;
             $crucero = $this->crucerosmodel->ActualizarCrucero($crucero_id, $nombre, $compania, $capacidad, $origen, $img1, $img2, $descripcion, $detalles);
             $this->crucerosview->response("Crucero id=$crucero_id actualizado con éxito", 200);
         } else
--- Models/Cruceros.Model.php
+++ Models/Cruceros.Model.php
@@ -58,4 +58,55 @@ class CrucerosModel
         $query = $this->db->prepare('UPDATE cruceros SET nombre=?, compania=?, capacidad=?, origen=?, img1=?, img2=?, descripcion=?, detalles=? WHERE ID = ?');
         $query->execute([$nombre, $compania, $capacidad, $origen, $img1, $img2, $descripcion, $detalles, $crucero_id]);
     }
+
+    /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*
+     |  Ordena los cruceros. |
+     *-----------------------*/
+    public function GetSortedCruceros($criterio, $orden)
+    {
+        $query = $this->db->prepare("SELECT * FROM cruceros ORDER BY $criterio $orden");
+        $query->execute();
+        $cruceros = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $cruceros;
+    }
+
+    /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*
+     | Obtiene columnas de la tabla. |
+     *-------------------------------*/
+    public function obtenerColumnas()
+    {
+        $sentencia = $this->db->prepare('SELECT column_name FROM information_schema.columns WHERE table_name = :table_name');
+        $sentencia->execute([':table_name' => 'cruceros']);
+        return $sentencia->fetchAll();
+    }
+
+    /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*
+     |  Filtrar los cruceros. |
+     *------------------------*/
+    public function GetFilteredCruceros($filtro, $valor)
+    {
+        $sql = "SELECT * FROM cruceros WHERE $filtro = :valor";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':valor', $valor);
+        $stmt->execute();
+        $cruceros = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $cruceros;
+    }
+
+    public function getPaginatedCruceros($inicio, $filas)
+    {
+        $sql = "SELECT * FROM cruceros LIMIT $inicio, $filas";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $cruceros = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $cruceros;
+    }
+
+    public function getTotalRecords()
+    {
+        $query = $this->db->prepare("SELECT count(*) FROM cruceros");
+        $query->execute();
+        return $query->fetchColumn();
+    }
+
 }
